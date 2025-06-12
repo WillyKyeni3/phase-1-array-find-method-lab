aacad1fb10787fe6dba8f78d06e6db2e6c14360e
willymusyoka <willykyeni@gmail.com>
2025-06-12 08:46:15 +0300

    Adds find-method

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+]
+
+// .find function that test each object to see if result is "w"
+function superbowlWin(record){
+    const winObj= record.find(game => game.result === "W");
+    if(winObj){
+        return winObj.year;
+    }else{
+        return undefined
+    }
+   
+    
+    // (function(item){
+    //     if( item.result === "W"){
+    //         return winObj.year;
+    //     }
+    // })
+}
+superbowlWin(record);
